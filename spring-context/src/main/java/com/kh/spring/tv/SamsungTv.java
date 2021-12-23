b928babcf8715fdf606c27784e5d723748fa4133
SOYOUNG <sysy931207@gmail.com>
2021-12-23 21:40:39 +0900

    12/23

---
+++ spring-context/src/main/java/com/kh/spring/tv/SamsungTv.java
@@ -0,0 +1,33 @@
+package com.kh.spring.tv;
+
+public class SamsungTv implements Tv {
+	
+	public RemoteControl getRemocon() {
+		return remocon;
+	}
+
+	public void setRemocon(RemoteControl remocon) {
+		this.remocon = remocon;
+	}
+
+	private RemoteControl remocon;
+	
+	public SamsungTv() {
+		System.out.println("SamsungTv객체를 생성합니다.");
+	}
+
+	public SamsungTv(RemoteControl remocon) {
+		this.remocon = remocon;
+	}
+
+	@Override
+	public void powerOn() {
+		System.out.println("SamsungTv의 전원을 켰습니다.");
+	}
+
+	@Override
+	public void changeChannel(int no) {
+		remocon.changeChannel(no);
+	}
+
+}
